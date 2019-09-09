e1047f19fa26d3f57b6b89d2dc8e9da641c6a242
Kirill Shevtsov <kirwork.em@gmail.com>
2019-09-09 19:18:38 +0300

    Creating configureStore function. Adding imports

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/rootReducer';
+import loggerMiddleware from '../middleware/logger';
 
 import { chooseUSDCurrency } from '../actions/chooseCurrency';
 
-const store = createStore(rootReducer);
+function configureStore(preloadedState) {
+  const middlewares = [loggerMiddleware, thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-const dis = store.dispatch(chooseUSDCurrency);
-console.log(dis);
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+  return store;
+}
+
+const store = configureStore();
+
+// const dis = store.dispatch(rootReducer);
+// console.log(dis);
 
 export default store;
