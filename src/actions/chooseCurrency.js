cce9ab2570766883cf0035fe93ab2eeadadf4d03
Kirill Shevtsov <kirwork.em@gmail.com>
2019-09-09 15:52:06 +0300

    Making action creator for UAH currency

--- src/actions/chooseCurrency.js
+++ src/actions/chooseCurrency.js
@@ -1,12 +1,14 @@
 import { CHOOSE_UAH_CURRENCY, CHOOSE_USD_CURRENCY, CHOOSE_EUR_CURRENCY } from '../constants/actionTypes';
 
-export const chooseUAHCurrency = {
-  type: CHOOSE_UAH_CURRENCY,
-  payload: {
-    currencyHTMLEntitySymbol: '&#8372;',
-    checked: true,
-  },
-};
+export function chooseUAHCurrency() {
+  return {
+    type: CHOOSE_UAH_CURRENCY,
+    payload: {
+      currencyHTMLEntitySymbol: '&#8372;',
+      checked: true,
+    },
+  };
+}
 
 export const chooseUSDCurrency = {
   type: CHOOSE_USD_CURRENCY,
