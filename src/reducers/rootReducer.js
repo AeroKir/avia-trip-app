89efe90fbe06ec7520fd11cbb6b9814cdeac5f88
Kirill Shevtsov <kirwork.em@gmail.com>
2019-09-09 19:21:34 +0300

    Removing formReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import showFlights from './showFlights';
 import chooseCurrencyReducer from './chooseCurrencyReducer';
 
 const rootReducer = combineReducers({
-  formReducer,
   chooseCurrencyReducer,
   showFlights,
 });
