37ee38e3ac217fe8d5739eb51c1378276c4116a1
Kirill Shevtsov <kirwork.em@gmail.com>
2019-09-09 16:18:14 +0300

    Adding formReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import showFlights from './showFlights';
 import chooseCurrencyReducer from './chooseCurrencyReducer';
 
 const rootReducer = combineReducers({
+  formReducer,
   chooseCurrencyReducer,
   showFlights,
 });
