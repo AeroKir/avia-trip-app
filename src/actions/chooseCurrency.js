048b74358f8f1ddd7b968a3026fc3410d4875a55
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-08 20:43:23 +0300

    Removing unusual actions

--- src/actions/chooseCurrency.js
+++ src/actions/chooseCurrency.js
@@ -1,39 +1,9 @@
 import { CHOOSE_CURRENCY } from '../constants/actionTypes';
 
-export function chooseUAHCurrency() {
-  return {
-    type: CHOOSE_UAH_CURRENCY,
-    payload: {
-      currencyHTMLEntitySymbol: '&#8372;',
-      checked: true,
-    },
-  };
-}
-
-export function chooseUSDCurrency() {
-  return {
-    type: CHOOSE_USD_CURRENCY,
-    payload: {
-      currencyHTMLEntitySymbol: '&#36;',
-      checked: true,
-    },
-  };
-}
-
-export function chooseEURCurrency() {
-  return {
-    type: CHOOSE_EUR_CURRENCY,
-    payload: {
-      currencyHTMLEntitySymbol: '&#8364;',
-      checked: true,
-    },
-  };
-}
-
-function chooseCurrency(newState) {
+function chooseCurrency(currency) {
   return {
     type: CHOOSE_CURRENCY,
-    payload: newState,
+    payload: currency,
   };
 }
 
