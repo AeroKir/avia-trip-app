7512e925ff8bc1a72fde8b86342a4ae3317d81f9
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-08 20:38:34 +0300

    Adding chooseCurrency action

--- src/actions/chooseCurrency.js
+++ src/actions/chooseCurrency.js
@@ -1,34 +1,34 @@
 import { CHOOSE_CURRENCY } from '../constants/actionTypes';
 
-// export function chooseUAHCurrency() {
-//   return {
-//     type: CHOOSE_UAH_CURRENCY,
-//     payload: {
-//       currencyHTMLEntitySymbol: '&#8372;',
-//       checked: true,
-//     },
-//   };
-// }
+export function chooseUAHCurrency() {
+  return {
+    type: CHOOSE_UAH_CURRENCY,
+    payload: {
+      currencyHTMLEntitySymbol: '&#8372;',
+      checked: true,
+    },
+  };
+}
 
-// export function chooseUSDCurrency() {
-//   return {
-//     type: CHOOSE_USD_CURRENCY,
-//     payload: {
-//       currencyHTMLEntitySymbol: '&#36;',
-//       checked: true,
-//     },
-//   };
-// }
+export function chooseUSDCurrency() {
+  return {
+    type: CHOOSE_USD_CURRENCY,
+    payload: {
+      currencyHTMLEntitySymbol: '&#36;',
+      checked: true,
+    },
+  };
+}
 
-// export function chooseEURCurrency() {
-//   return {
-//     type: CHOOSE_EUR_CURRENCY,
-//     payload: {
-//       currencyHTMLEntitySymbol: '&#8364;',
-//       checked: true,
-//     },
-//   };
-// }
+export function chooseEURCurrency() {
+  return {
+    type: CHOOSE_EUR_CURRENCY,
+    payload: {
+      currencyHTMLEntitySymbol: '&#8364;',
+      checked: true,
+    },
+  };
+}
 
 function chooseCurrency(newState) {
   return {
