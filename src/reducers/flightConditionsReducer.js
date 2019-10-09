9bd6fc735b32574b247033ac3e62b01bdace4d3e
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-09 21:42:51 +0300

    Creating flightConditionsReduser

---
+++ src/reducers/flightConditionsReducer.js
@@ -0,0 +1,20 @@
+import { FLIGHT_CONDITION_CHOICE } from '../constants/actionTypes';
+import initialState from '../store/initialState';
+
+function flightConditionsReducer(state = initialState, action) {
+  switch (action.type) {
+    case FLIGHT_CONDITION_CHOICE:
+      const newFligtCondition = state.flightSearchConditions.map((item) => {
+        if (item.value === action.payload.value) {
+          return Object.assign({}, item, { checked: action.payload.checked });
+        }
+        return item;
+      });
+      const newFlightConditionState = Object.assign({}, state, { flightSearchConditions: newFligtCondition });
+      return newFlightConditionState;
+    default:
+      return state;
+  }
+}
+
+export default flightConditionsReducer;
