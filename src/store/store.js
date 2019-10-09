8786916126a73b153050bc726dee83ccd38ad405
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-09 21:34:56 +0300

    Removing unused code

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,8 @@ import { applyMiddleware, compose, createStore, bindActionCreators } from 'redux
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-// import initialState from './initialState';
-import { rootReducer } from '../reducers/rootReducer';
+import rootReducer from '../reducers/rootReducer';
 import loggerMiddleware from '../middleware/logger';
-
-import chooseCurrencyReducer from '../reducers/chooseCurrencyReducer';
-import { chooseUSDCurrency } from '../actions/chooseCurrency';
 
 // function configureStore(preloadedState) {
 //   const middlewares = [loggerMiddleware, thunkMiddleware];
@@ -23,34 +19,6 @@ import { chooseUSDCurrency } from '../actions/chooseCurrency';
 
 // const store = configureStore();
 
-// const initialState = {
-//   name: 'Ivan',
-//   secondName: 'Ivanov',
-// };
-
-// const changeName = {
-//   type: 'CHANGE_NAME',
-//   payload: 'Petya',
-// };
-
-// const changeSecondName = {
-//   type: 'CHANGE_SECOND_NAME',
-//   payload: 'Petrov',
-// };
-
-// function reducer(state = initialState, action) {
-//   switch (action.type) {
-//     case 'CHANGE_NAME':
-//       return { ...state, name: action.payload };
-
-//     case 'CHANGE_SECOND_NAME':
-//       return { ...state, secondName: action.payload };
-//   }
-//   return state;
-// }
-
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-// const dis = store.dispatch(rootReducer);
-// console.log(dis);
 
 export default store;
