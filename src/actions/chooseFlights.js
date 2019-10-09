fc298023ab953b37c55250723ebeae9871923bde
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-09 21:08:20 +0300

    Creating action

--- src/actions/chooseFlights.js
+++ src/actions/chooseFlights.js
@@ -1,39 +1,11 @@
-import {
-  CHOOSE_ALL_FLIGHTS,
-  CHOOSE_FLIGHTS_WITHOUT_STOPS,
-  CHOOSE_FLIGHTS_WITH_ONE_STOP,
-  CHOOSE_FLIGHTS_WITH_TWO_STOPS,
-  CHOOSE_FLIGHTS_WITH_THREE_STOPS,
-} from '../constants/actionTypes';
+function flightConditionChoice(checked, value) {
+  return {
+    type: 'FLIGHT_CONDITION_CHOICE',
+    payload: {
+      checked,
+      value,
+    },
+  };
+}
 
-export const chooseAllFlights = {
-  type: CHOOSE_ALL_FLIGHTS,
-};
-
-export const chooseFlightsWithoutStops = {
-  type: CHOOSE_FLIGHTS_WITHOUT_STOPS,
-  payload: {
-    stops: 0,
-  },
-};
-
-export const chooseFlightsWithOneStop = {
-  type: CHOOSE_FLIGHTS_WITH_ONE_STOP,
-  payload: {
-    stops: 1,
-  },
-};
-
-export const chooseFlightsWithTwoStops = {
-  type: CHOOSE_FLIGHTS_WITH_TWO_STOPS,
-  payload: {
-    stops: 2,
-  },
-};
-
-export const chooseFlightsWithThreeStops = {
-  type: CHOOSE_FLIGHTS_WITH_THREE_STOPS,
-  payload: {
-    stops: 3,
-  },
-};
+export default flightConditionChoice;
