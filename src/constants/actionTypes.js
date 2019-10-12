6457272987fb36239bc55d980c34e1474ed22494
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-12 19:21:51 +0300

    Adding currency convert action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,7 @@
 // Flights choosing
 export const FLIGHT_CONDITION_CHOICE = 'FLIGHT_CONDITION_CHOICE';
 
-// Currency choosing
+// Actions with currencies
 export const CHOOSE_CURRENCY = 'CHOOSE_CURRENCY';
+export const CONVERT_UAH_TO_USD = 'CONVERT_UAH_TO_USD';
+export const CONVERT_UAH_TO_EUR = 'CONVERT_UAH_TO_EUR';
