433265a0db4d724e27d229c71b350200ea97f07c
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-15 19:19:07 +0300

    Adding request flights action type

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,6 @@
-// Flights choosing
+// Actions with flights
 export const FLIGHT_CONDITION_CHOICE = 'FLIGHT_CONDITION_CHOICE';
+export const REQUEST_FLIGHTS = 'REQUEST_FLIGHTS';
 
 // Actions with currencies
 export const CHOOSE_CURRENCY = 'CHOOSE_CURRENCY';
