9e3760cd5d44cd646f6b3b399e1a1b4ae0571e7f
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-15 19:47:00 +0300

    Adding requestFlightReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import flightConditionsReducer from './flightConditionsReducer';
 import chooseCurrencyReducer from './chooseCurrencyReducer';
+import requestFlightsReducer from './requestFlightsReducer';
 
 const rootReducer = combineReducers({
   currency: chooseCurrencyReducer,
   flightSearchConditions: flightConditionsReducer,
+  flightVariants: requestFlightsReducer,
 });
 
 export default rootReducer;
