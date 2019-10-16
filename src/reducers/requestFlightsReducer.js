164b09b4d18e8eeb7ceb63800462f0ff76c5307d
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-16 19:42:06 +0300

    Adding mock case into reducer

--- src/reducers/requestFlightsReducer.js
+++ src/reducers/requestFlightsReducer.js
@@ -1,10 +1,15 @@
-import { REQUEST_FLIGHTS } from '../constants/actionTypes';
+import { REQUEST_FLIGHTS, RECEIVE_FLIGHTS_MOCK } from '../constants/actionTypes';
 import initialState from '../store/initialState';
 
 function requestFlightsReducer(state = initialState, action) {
   switch (action.type) {
     case REQUEST_FLIGHTS:
     {
+      const newFlightsState = Object.assign({}, state, { flightVariants: action.payload });
+      return newFlightsState;
+    }
+
+    case RECEIVE_FLIGHTS_MOCK: {
       const newFlightsState = Object.assign({}, state, { flightVariants: action.payload });
       return newFlightsState;
     }
