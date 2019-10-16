4304e5b4cf4f6157f08bacd89a892cfabe20d768
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-16 19:36:54 +0300

    Adding action creator for mock data

--- src/actions/requestFlights.js
+++ src/actions/requestFlights.js
@@ -1,10 +1,36 @@
-import { REQUEST_FLIGHTS } from '../constants/actionTypes';
+import { REQUEST_FLIGHTS, RECEIVE_FLIGHTS, RECEIVE_FLIGHTS_MOCK } from '../constants/actionTypes';
+import TICKETS from '../data/tickets-array';
 
-function requestFlights(flights) {
+// Action creator for mock data
+export function getFlightsMock(flights) {
+  return {
+    type: RECEIVE_FLIGHTS_MOCK,
+    payload: flights,
+  };
+}
+
+export function requestFlightsMock() {
+  return dispatch => dispatch(getFlightsMock(TICKETS));
+}
+
+export function requestFlights(flights) {
   return {
     type: REQUEST_FLIGHTS,
     payload: flights,
   };
 }
 
-export default requestFlights;
+export function fetchFlights(url) {
+  return (dispatch) => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(flights => dispatch(requestFlights(flights)))
+      .catch(() => {});
+  };
+}
