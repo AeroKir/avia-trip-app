db45e01974aa47c95ea958ccddaa2108eeb58b4a
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-16 19:38:28 +0300

    Adding mock action type

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,6 +1,8 @@
 // Actions with flights
 export const FLIGHT_CONDITION_CHOICE = 'FLIGHT_CONDITION_CHOICE';
 export const REQUEST_FLIGHTS = 'REQUEST_FLIGHTS';
+export const RECEIVE_FLIGHTS = 'RECEIVE_FLIGHTS';
+export const RECEIVE_FLIGHTS_MOCK = 'RECEIVE_FLIGHTS_MOCK';
 
 // Actions with currencies
 export const CHOOSE_CURRENCY = 'CHOOSE_CURRENCY';
