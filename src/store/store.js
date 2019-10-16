d43d2ebccff6ecdf0fafaf389ce4365a01a3eb92
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-16 17:30:43 +0300

    Adding composeWithDevTools and redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
-import { applyMiddleware, compose, createStore, bindActionCreators } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 import loggerMiddleware from '../middleware/logger';
+
+// import curRate from '../services/privatbank';
+import { fetchFlights } from '../actions/requestFlights';
+
+
+fetch('../data/tickets.json')
+  .then(response => console.log(response.json()));
+// .then(json => console.log(json));
+
+// const EXCHANGE_RATE_API_URL = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11';
+// fetch(EXCHANGE_RATE_API_URL)
+//   .then(response => response.json())
+//   .then(data => data);
 
 // function configureStore(preloadedState) {
 //   const middlewares = [loggerMiddleware, thunkMiddleware];
@@ -18,7 +31,7 @@ import loggerMiddleware from '../middleware/logger';
 // }
 
 // const store = configureStore();
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
