3f342a0d8dcca9bedbfdf940c95f2c9bbf2c7775
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-28 15:20:34 +0200

    Adding async actions

--- src/actions/convertCurrency.js
+++ src/actions/convertCurrency.js
@@ -1,15 +1,72 @@
 import { CONVERT_UAH_TO_USD, CONVERT_UAH_TO_EUR } from '../constants/actionTypes';
 
-export function convertUAHToUSD(UAHToUSD) {
+export function convertUAHToUSD(UAHPerUSD, value) {
   return {
     type: CONVERT_UAH_TO_USD,
-    payload: UAHToUSD,
+    payload: { UAHPerUSD, value },
   };
 }
 
-export function convertUAHToEUR(UAHToEUR) {
+export function convertUAHToEUR(UAHPerEUR, value) {
   return {
     type: CONVERT_UAH_TO_EUR,
-    payload: UAHToEUR,
+    payload: { UAHPerEUR, value },
+  };
+}
+
+export function fetchUSDSaleRate(url) {
+  return (dispatch) => {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    fetch(proxyUrl + url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then((currencyData) => {
+        const currencySale = currencyData.map((currency) => {
+          if (currency.ccy === 'USD') {
+            return Number(currency.sale);
+          }
+          return currency;
+        });
+        return currencySale;
+      })
+      .then((currencySale) => {
+        const USDSaleRate = currencySale[0];
+        dispatch(convertUAHToUSD(USDSaleRate));
+      })
+      .catch(() => { });
+  };
+}
+
+
+export function fetchEURSaleRate(url) {
+  return (dispatch) => {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    fetch(proxyUrl + url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then((currencyData) => {
+        const currencySale = currencyData.map((currency) => {
+          if (currency.ccy === 'EUR') {
+            return Number(currency.sale);
+          }
+          return currency;
+        });
+        return currencySale;
+      })
+      .then((currencySale) => {
+        const EURSaleRate = currencySale[1];
+        dispatch(convertUAHToEUR(EURSaleRate));
+      })
+      .catch(() => { });
   };
 }
