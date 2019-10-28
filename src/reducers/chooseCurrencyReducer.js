b9c0604ac5f3f534919501c52db2b272fdfbabe2
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-28 15:15:08 +0200

    Adding currency converting

--- src/reducers/chooseCurrencyReducer.js
+++ src/reducers/chooseCurrencyReducer.js
@@ -1,19 +1,60 @@
-import { CHOOSE_CURRENCY } from '../constants/actionTypes';
+import { CHOOSE_CURRENCY, CONVERT_UAH_TO_USD, CONVERT_UAH_TO_EUR } from '../constants/actionTypes';
 import initialState from '../store/initialState';
+
+// const initialPrice = initialState.flightVariants.map(item => item.price);
+// const initialStateFlights = initialState.flightVariants;
 
 function chooseCurrencyReducer(state = initialState, action) {
   switch (action.type) {
     case CHOOSE_CURRENCY:
     {
       const newCurrency = state.currency.map((item) => {
-        if (item.value === action.payload) {
+        if (item.value === action.payload.currency) {
           return Object.assign({}, item, { checked: true });
         }
         return Object.assign({}, item, { checked: false });
       });
+
       const newCurrencyState = Object.assign({}, state, { currency: newCurrency });
       return newCurrencyState;
     }
+
+    case CONVERT_UAH_TO_USD:
+    {
+      const choosedCurrency = state.currency.map((item) => {
+        if (item.value === action.payload.value) {
+          return initialState.flightVariants.map(flight => Object.assign(
+            {},
+            flight,
+            { price: flight.price / action.payload.UAHPerUSD },
+          ));
+        }
+        return initialState.flightVariants;
+      });
+
+      return Object.assign({}, state, { flightVariants: choosedCurrency[1] });
+    }
+
+    case CONVERT_UAH_TO_EUR:
+    {
+      const choosedCurrency = state.currency.map((item) => {
+        if (item.value === action.payload.value) {
+          return initialState.flightVariants.map(flight => Object.assign(
+            {},
+            flight,
+            { price: flight.price / action.payload.UAHPerEUR },
+          ));
+        }
+        return state.flightVariants.map(flight => Object.assign(
+          {},
+          flight,
+          { price: flight.price },
+        ));
+      });
+
+      return Object.assign({}, state, { flightVariants: choosedCurrency[2] });
+    }
+
     default:
       return state;
   }
