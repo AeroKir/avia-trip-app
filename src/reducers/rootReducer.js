aec8cbc4bdc7f518ade4e7bc17a95e0e3d2478b6
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-28 15:25:18 +0200

    Changing reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 import flightConditionsReducer from './flightConditionsReducer';
 import chooseCurrencyReducer from './chooseCurrencyReducer';
 import requestFlightsReducer from './requestFlightsReducer';
+import convertCurrencyReducer from './convertCurrencyReducer';
 
 const rootReducer = combineReducers({
   currency: chooseCurrencyReducer,
   flightSearchConditions: flightConditionsReducer,
-  flightVariants: requestFlightsReducer,
+  flightVariants: chooseCurrencyReducer,
 });
 
 export default rootReducer;
