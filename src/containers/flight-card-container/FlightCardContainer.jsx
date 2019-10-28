927f8c234f1af02629d26538274c356cdf1f7412
Kirill Shevtsov <kirwork.em@gmail.com>
2019-10-28 15:12:53 +0200

    Separating on constants airline logo url

--- src/containers/flight-card-container/FlightCardContainer.jsx
+++ src/containers/flight-card-container/FlightCardContainer.jsx
@@ -7,24 +7,47 @@ import ListGroupItem from '../../components/list-group/ListGroupItem';
 import FlightCard from '../../components/flight-card/FlightCard';
 import Icon from '../../components/icon/Icon';
 import { requestFlightsMock, receiveAirlineLogo, fetchAirlineLogo } from '../../actions/requestFlights';
+import { fetchUSDSaleRate, fetchEURSaleRate, convertUAHToUSD } from '../../actions/convertCurrency';
+import chooseCurrency from '../../actions/chooseCurrency';
 
 class FlightCardContainer extends React.Component {
-  componentDidMount() {
-    const { fetchData } = this.props;
-    fetchData();
+  // componentDidMount() {
+  //   const { fetchData, convertPriceToUSD, convertPriceToEUR } = this.props;
+  //   fetchData();
+  //   convertPriceToUSD('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
+  //   convertPriceToEUR('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
+  // }
+
+  componentDidUpdate(prevProps) {
+    const { flightVariants } = this.props;
+    if (prevProps.flightVariants !== flightVariants) {
+      flightVariants.map(
+        (i) => {
+          console.log(i.price / 25);
+          return i.price;
+        },
+      );
+    }
   }
 
   render() {
-    const { currency, flightVariants } = this.props;
+    const {
+      currency, flightVariants,
+    } = this.props;
+
+    const airlineLogoUrl = 'https://daisycon.io/images/airline/';
+    const airlineLogoWidth = '300';
+    const airlineLogoHeight = '150';
+    const airlineLogoBgColor = 'ffffff';
 
     return (
       <div>
         {flightVariants.map(item => (
           <FlightCard
-            src={`https://daisycon.io/images/airline/?width=300&height=150&color=ffffff&iata=${item.carrier}`}
+            src={`${airlineLogoUrl}?width=${airlineLogoWidth}&height=${airlineLogoHeight}&color=${airlineLogoBgColor}&iata=${item.carrier}`}
             price={item.price}
             currencySymbol={currency.map(
-              i => i.checked && i.currencyUnicodeSymbol
+              i => i.checked && i.currencyUnicodeSymbol,
             )}
             origin={item.origin}
             originName={item.origin_name}
@@ -53,7 +76,11 @@ function mapStateToProps(store) {
 
 function mapDispatchToProps(dispatch) {
   return {
+    chooseCurrency: bindActionCreators(chooseCurrency, dispatch),
     fetchData: () => dispatch(requestFlightsMock()),
+    // convertPriceToUSD: url => dispatch(fetchUSDSaleRate(url)),
+    // convertPriceToEUR: url => dispatch(fetchEURSaleRate(url)),
+    // convertUAHToUSD: price => dispatch(convertUAHToUSD(price)),
   };
 }
 
