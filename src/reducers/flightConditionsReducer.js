d3c988e6484eb086c799f6ac39aae1625fde9feb
Kirill Shevtsov <kirwork.em@gmail.com>
2019-11-06 18:47:15 +0200

    Adding new handlers

--- src/reducers/flightConditionsReducer.js
+++ src/reducers/flightConditionsReducer.js
@@ -5,10 +5,59 @@ function flightConditionsReducer(state = initialState, action) {
   switch (action.type) {
     case FLIGHT_CONDITION_CHOICE:
     {
+      const allFlightsItem = state.flightSearchConditions.filter(i => i.value === 'transfer-all');
+      console.log(allFlightsItem);
+
+      const otherFlights = state.flightSearchConditions.filter(i => i.value !== 'transfer-all');
+      console.log(otherFlights);
+
+      const all = allFlightsItem.concat(otherFlights);
+      console.log(all);
+
+
       const newFligtCondition = state.flightSearchConditions.map((item) => {
+        // Common case
         if (item.value === action.payload.value) {
           return Object.assign({}, item, { checked: action.payload.checked });
         }
+
+        // if (action.payload.value === 'transfer-all' && item.checked !== action.payload.checked) {
+        //   return Object.assign(
+        //     {},
+        //     item,
+        //     {
+        //       checked: action.payload.checked,
+        //     },
+        //   );
+        // }
+
+        // 1-st case
+        if (action.payload.value === 'transfer-all') {
+          return Object.assign(
+            {},
+            item,
+            { checked: action.payload.checked },
+          );
+        }
+
+        // 2-nd case
+        if (action.payload.value !== 'transfer-all' && item.value === 'transfer-all' && item.checked) {
+          return Object.assign(
+            {},
+            item,
+            { checked: action.payload.checked },
+          );
+        }
+
+        // if (item.checked && item.value !== 'transfer-all' && action.payload.value === item.value) {
+        //   return Object.assign(
+        //     {},
+        //     item,
+        //     { checked: action.payload.checked },
+        //   );
+        // }
+        
+        console.log(item);
         return item;
       });
       const newFlightConditionState = Object.assign({}, state, { flightSearchConditions: newFligtCondition });
