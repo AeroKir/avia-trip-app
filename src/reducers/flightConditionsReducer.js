94bd56ac15eccab7413d868327a4be1603b2af2d
Kirill Shevtsov <kirwork.em@gmail.com>
2019-11-10 17:53:35 +0200

    Adding new handler and comments

--- src/reducers/flightConditionsReducer.js
+++ src/reducers/flightConditionsReducer.js
@@ -5,33 +5,17 @@ function flightConditionsReducer(state = initialState, action) {
   switch (action.type) {
     case FLIGHT_CONDITION_CHOICE:
     {
-      const allFlightsItem = state.flightSearchConditions.filter(i => i.value === 'transfer-all');
-      console.log(allFlightsItem);
-
-      const otherFlights = state.flightSearchConditions.filter(i => i.value !== 'transfer-all');
-      console.log(otherFlights);
-
-      const all = allFlightsItem.concat(otherFlights);
-      console.log(all);
-
-
       const newFligtCondition = state.flightSearchConditions.map((item) => {
         // Common case
         if (item.value === action.payload.value) {
           return Object.assign({}, item, { checked: action.payload.checked });
         }
 
-        // if (action.payload.value === 'transfer-all' && item.checked !== action.payload.checked) {
-        //   return Object.assign(
-        //     {},
-        //     item,
-        //     {
-        //       checked: action.payload.checked,
-        //     },
-        //   );
-        // }
-
-        // 1-st case
+        /**
+         * 1-st case.
+         * If the 'transfer-all' item is checked, each other will be checked.
+         * And vice versa.
+         */
         if (action.payload.value === 'transfer-all') {
           return Object.assign(
             {},
@@ -40,7 +24,10 @@ function flightConditionsReducer(state = initialState, action) {
           );
         }
 
-        // 2-nd case
+        /**
+         * 2-nd case.
+         * If at least one item except 'transfer-all' is unchecked - 'transfer-all' marked as unchecked.
+         */
         if (action.payload.value !== 'transfer-all' && item.value === 'transfer-all' && item.checked) {
           return Object.assign(
             {},
@@ -49,18 +36,29 @@ function flightConditionsReducer(state = initialState, action) {
           );
         }
 
-        // if (item.checked && item.value !== 'transfer-all' && action.payload.value === item.value) {
-        //   return Object.assign(
-        //     {},
-        //     item,
-        //     { checked: action.payload.checked },
-        //   );
-        // }
-        
-        console.log(item);
+        /**
+         * 3-rd case.
+         * If all items except 'transfer-all' item are checked,
+         * and 'transfer-all' unchecked at that moment - set 'transfer-all' checked = true.
+         */
+        const allFlightsItem = state.flightSearchConditions.find(i => i.value === 'transfer-all');
+        const otherFlightsVariants = state.flightSearchConditions.filter(i => i.value !== 'transfer-all');
+        const isOtherFlightsChecked = otherFlightsVariants.filter(i => i.checked);
+
+        if (action.payload.value !== 'transfer-all'
+            && action.payload.checked
+            && isOtherFlightsChecked.length === otherFlightsVariants.length - 1) {
+          allFlightsItem.checked = true;
+        }
+
         return item;
       });
-      const newFlightConditionState = Object.assign({}, state, { flightSearchConditions: newFligtCondition });
+
+      const newFlightConditionState = Object.assign(
+        {},
+        state,
+        { flightSearchConditions: newFligtCondition },
+      );
       return newFlightConditionState;
     }
     default:
