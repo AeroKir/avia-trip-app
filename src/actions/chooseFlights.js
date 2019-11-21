19ae1147b269850fb9f1ce08d21c6839a80c3979
Kirill Shevtsov <kirwork.em@gmail.com>
2019-11-21 16:58:07 +0200

    Adding const import

--- src/actions/chooseFlights.js
+++ src/actions/chooseFlights.js
@@ -1,6 +1,8 @@
+import { FLIGHT_CONDITION_CHOICE } from '../constants/actionTypes';
+
 function flightConditionChoice(checked, value) {
   return {
-    type: 'FLIGHT_CONDITION_CHOICE',
+    type: FLIGHT_CONDITION_CHOICE,
     payload: {
       checked,
       value,
