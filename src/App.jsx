0c749e83397c51081d52c0ae663516995b065210
Kirill Shevtsov <kirwork.em@gmail.com>
2019-11-22 16:55:27 +0200

    Simplifying App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
 import React from 'react';
-import Icon from './components/icon/Icon';
-import Aside from './layout/aside/Aside';
-import Main from './layout/main/Main';
-import FlightStopsFilterContainer from './containers/filters-container/FlightStopsFilterContainer';
-import CurrencyBlockContainer from './containers/currency-block-container/CurrencyBlockContainer';
-import FlightCardContainer from './containers/flight-card-container/FlightCardContainer';
-
-import './App.css';
-
-import iconLogo from './icons/globe.svg';
+import AppLayout from './layout/app-layout/AppLayout';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Icon
-        className="app-logo"
-        src={iconLogo}
-        alt="App logo icon"
-      />
-      <div className="row">
-        <Aside>
-          <fieldset className="currency-select-field">
-            <legend>Валюта</legend>
-            <CurrencyBlockContainer />
-          </fieldset>
-          <fieldset className="transfer-select-field check-transfer-list">
-            <legend className="transfer-filter-legend">Количество пересадок</legend>
-            <FlightStopsFilterContainer />
-          </fieldset>
-        </Aside>
-        <Main>
-          <FlightCardContainer />
-        </Main>
-      </div>
-    </div>
+    <AppLayout />
   );
 }
 
