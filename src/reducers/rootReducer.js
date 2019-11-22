fae54125465bac5b6e590296ea252496fd24dcb3
Kirill Shevtsov <kirwork.em@gmail.com>
2019-11-22 17:02:02 +0200

    Adding sortFlightsReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import flightConditionsReducer from './flightConditionsReducer';
 import chooseCurrencyReducer from './chooseCurrencyReducer';
+import sortFlightsReducer from './sortFlightsReducer';
 import requestFlightsReducer from './requestFlightsReducer';
 import convertCurrencyReducer from './convertCurrencyReducer';
 
 const rootReducer = combineReducers({
   currency: chooseCurrencyReducer,
   flightSearchConditions: flightConditionsReducer,
+  flightSortFilter: sortFlightsReducer,
   flightVariants: requestFlightsReducer,
 });
 
