809c04f3ecde15cfba9b1f49176174cf73c38410
Kirill Shevtsov <kirwork.em@gmail.com>
2024-01-30 15:52:25 +0200

    Updates

---
+++ precache-manifest.1bb6c6cd93d1dd3040a23a7757175a1f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc668140f68f60304d09d9d0f6078402",
+    "url": "/avia-trip-app/index.html"
+  },
+  {
+    "revision": "75277bd245901b036352",
+    "url": "/avia-trip-app/static/css/main.9db21b72.chunk.css"
+  },
+  {
+    "revision": "fa9dfd03b3fc1ac94c19",
+    "url": "/avia-trip-app/static/js/2.05eeeae4.chunk.js"
+  },
+  {
+    "revision": "75277bd245901b036352",
+    "url": "/avia-trip-app/static/js/main.8c24729b.chunk.js"
+  },
+  {
+    "revision": "17651446d69e53989c72",
+    "url": "/avia-trip-app/static/js/runtime~main.adfdfd23.js"
+  },
+  {
+    "revision": "6260d5de6d9b820c67bb56f67eb13a06",
+    "url": "/avia-trip-app/static/media/Air_France_Logo.6260d5de.svg"
+  },
+  {
+    "revision": "d3fb9c6bdc2a42879a053e333bd48643",
+    "url": "/avia-trip-app/static/media/Logo_of_El_Al_Israel_Airlines.d3fb9c6b.svg"
+  },
+  {
+    "revision": "98c36676122f48684b1595fd7b336dd3",
+    "url": "/avia-trip-app/static/media/Ryanair.98c36676.svg"
+  },
+  {
+    "revision": "b9c49c691e6b8df352fee663efec4443",
+    "url": "/avia-trip-app/static/media/check-arrow.b9c49c69.svg"
+  },
+  {
+    "revision": "951e168287fc41f8e29f790be634bbd8",
+    "url": "/avia-trip-app/static/media/globe.951e1682.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avia-trip-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4cd46e0744dce6dd7fdcf7ff5580d7b8",
+    "url": "/avia-trip-app/static/media/lufthansa-logo.4cd46e07.svg"
+  },
+  {
+    "revision": "e78481ea06df421af4634c2a2ecbe2c9",
+    "url": "/avia-trip-app/static/media/plane-gray.e78481ea.svg"
+  },
+  {
+    "revision": "1738e37aa14ea9d049ed9c17a4a34a1e",
+    "url": "/avia-trip-app/static/media/radio-check-dot.1738e37a.svg"
+  },
+  {
+    "revision": "b48b60ba57f61f908d487bddc8655115",
+    "url": "/avia-trip-app/static/media/turkish-airlines-logo-v1.b48b60ba.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/avia-trip-app/precache-manifest.774559fb7d164da2115c9c226e316312.js"
+  "/avia-trip-app/precache-manifest.1bb6c6cd93d1dd3040a23a7757175a1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avia-trip-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
